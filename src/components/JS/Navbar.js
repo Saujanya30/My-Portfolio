a46a9ec7427b3bced43d074629301f1a84fe9055
Saujanya <solankisaujanya@gmail.com>
2023-11-19 23:53:41 +0530

    fixed nav link from url location

--- src/components/JS/Navbar.js
+++ src/components/JS/Navbar.js
@@ -1,16 +1,31 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import '../CSS/Navbar.css';
 import logo from '../Files/portfolio_logo.png';
+
 export default function Navbar() {
+  const [activeTab, setActiveTab] = useState("Home");
+  const location = useLocation();
+  useEffect(() => {
+    if (location.pathname === "/") {
+      setActiveTab("Home");
+    }
+    else if (location.pathname === "Project") {
+      setActiveTab("Projects");
+    }
+    else if (location.pathname === "About") {
+      setActiveTab("About");
+    }
+  }, [location]);
+
   return (
     <div className="nav">
-        <img src={logo} alt="oops" className='logo'/>
-        <ul className="nav_elements">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/Project">Projects</Link></li>
-        <li><Link to="/About">About Me</Link></li>
-        </ul>
+      <img src={logo} alt="oops" className='logo' />
+      <ul className="nav_elements">
+        <Link to="/"><li className={`${activeTab === "Home" ? 'active' : ''}`} onClick={() => setActiveTab('Home')}>Home</li></Link>
+        <Link to="/Project"><li className={`${activeTab === "Projects" ? 'active' : ''}`} onClick={() => setActiveTab('Projects')}>Projects</li></Link>
+        <Link to="/About"><li className={`${activeTab === "About" ? 'active' : ''}`} onClick={() => setActiveTab('About')}>About Me</li></Link>
+      </ul>
     </div>
   )
 }
